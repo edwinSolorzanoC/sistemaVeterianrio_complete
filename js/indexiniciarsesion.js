25df6ec17923912df2726fa52b034ef7e1f5a4c5
edwin solorzano <edwinuni26@gmail.com>
2024-12-06 16:25:41 -0600

    nuevo index

---
+++ js/indexiniciarsesion.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const btnIniciarSesion = document.getElementById('iniciarsesion');
+
+    btnIniciarSesion.addEventListener('click', function(){
+
+        window.location.href = 'administracion.html';
+
+
+    })
+
+
+
+})
