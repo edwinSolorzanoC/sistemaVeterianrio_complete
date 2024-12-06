2c03fc749598bcfef100a683634502b565513e6e
edwin solorzano <edwinuni26@gmail.com>
2024-12-06 17:10:55 -0600

    continuamos con los links

---
+++ js/perfilpacientes.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const volver = document.getElementById("volver");
+
+    volver.addEventListener('click', function(){
+        window.location.href = 'administracion.html';
+    });
+
+})
