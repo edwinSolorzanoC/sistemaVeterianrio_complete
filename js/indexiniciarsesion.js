f8d969f9c838036184728dc8535797a356dd704d
edwin solorzano <edwinuni26@gmail.com>
2024-12-11 23:29:52 -0600

    cambios de index y en el header

---
+++ js/indexiniciarsesion.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const iniciarSesion = document.getElementById("btn-iniciarSesion");
+
+    iniciarSesion.addEventListener('click', function(){
+        window.location.href = 'administracion.html';
+    })
+})
