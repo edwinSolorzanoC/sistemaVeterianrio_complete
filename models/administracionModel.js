f5beaa6b8e7516417051641a2d41ba4d5b1373ae
edwin solorzano <edwinuni26@gmail.com>
2025-01-15 16:01:38 -0600

    ya se hace validacion para insertar consulta con o sin paciente

--- models/administracionModel.js
+++ models/administracionModel.js
@@ -19,32 +19,82 @@ administracionModel.consultaGeneralSinPaciente = (nombrePropietarioConsulta,
     medicamentosConsulta,
     pesoConsultaGeneral,
     fechaAutomatica,
-    idVeterinaria,
-     callback) => {
-    
-    const peticion = `INSERT INTO tb_consultageneral (tb_consultaGeneral_col_nombrePropietario, 
-        tb_consultaGeneral_col_nombrePaciente,
-        tb_consultaGeneral_col_motivo, 
-        tb_consultaGeneral_col_medicamentosUtilizados, 
-        tb_consultaGeneral_col_actualizacionPeso,
-        tb_consultaGeneral_col_fecha, 
-        tb_usuariosVeterinaria_idtb_usuariosVeterinaria)
-        VALUES(?,
-        ?,
-        ?,
-        ?,
-        ?,
-        ?,
-        ?
-        );`;
+    idVeterinaria, callback) => {
+        
+        const verificarExistenciaPacientes = `SELECT  idtb_pacientes
+        FROM tb_pacientes
+        JOIN tb_propietarios
+        ON tb_propietarios_tb_propietarios_col_cedula = tb_propietarios.tb_propietarios_col_cedula
+        WHERE tb_pacientes_col_nombre = ? && tb_propietarios_col_nombre = ?;`;
 
-        connection.query(peticion, [nombrePropietarioConsulta,
-            nombrePacienteConsulta,
-            motivoConsulta,
-            medicamentosConsulta,
-            pesoConsultaGeneral,
-            fechaAutomatica,
-            idVeterinaria], callback);
+        connection.query(verificarExistenciaPacientes,[nombrePacienteConsulta,nombrePropietarioConsulta], (error, results) => {
+            if(error){
+                console.log("error en la consulta de existencia de pacientes");
+            }else{
+                
+                if(results.length > 0){
+                    console.log("si se ejcutó el select y si se encontro paciente");
+                    const idMascota = results[0].idtb_pacientes;
+
+                    const peticionConMascota = `INSERT INTO tb_consultageneral (
+                    tb_consultaGeneral_col_nombrePropietario, 
+                    tb_consultaGeneral_col_nombrePaciente,
+                    tb_consultaGeneral_col_motivo, 
+                    tb_consultaGeneral_col_medicamentosUtilizados, 
+                    tb_consultaGeneral_col_actualizacionPeso,
+                    tb_consultaGeneral_col_fecha, 
+                    tb_usuariosVeterinaria_idtb_usuariosVeterinaria,
+                    tb_pacientes_idtb_pacientes)
+                    VALUES(
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?
+                    );`;
+
+                    connection.query(peticionConMascota, [nombrePropietarioConsulta,
+                        nombrePacienteConsulta,
+                        motivoConsulta,
+                        medicamentosConsulta,
+                        pesoConsultaGeneral,
+                        fechaAutomatica,
+                        idVeterinaria,
+                        idMascota], callback);
+                }else{
+                    console.log("si se ejcutó el select y no se encontro paciente")
+                    const peticion = `INSERT INTO tb_consultageneral (tb_consultaGeneral_col_nombrePropietario, 
+                    tb_consultaGeneral_col_nombrePaciente,
+                    tb_consultaGeneral_col_motivo, 
+                    tb_consultaGeneral_col_medicamentosUtilizados, 
+                    tb_consultaGeneral_col_actualizacionPeso,
+                    tb_consultaGeneral_col_fecha, 
+                    tb_usuariosVeterinaria_idtb_usuariosVeterinaria)
+                    VALUES(?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?,
+                    ?
+                    );`;
+            
+                    connection.query(peticion, [nombrePropietarioConsulta,
+                        nombrePacienteConsulta,
+                        motivoConsulta,
+                        medicamentosConsulta,
+                        pesoConsultaGeneral,
+                        fechaAutomatica,
+                        idVeterinaria], callback);
+                    
+                }
+            }
+        })
+        
+        
 }
 
 administracionModel.consultaVacunacionSinPaciente = (
