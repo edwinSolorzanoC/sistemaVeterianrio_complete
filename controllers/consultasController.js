1680b9fdcf47843198d2c6ab88f107f725cf38c5
edwin solorzano <edwinuni26@gmail.com>
2025-02-05 23:30:44 -0600

    cambios en modeulo consultas/consulta inicio, ahora es asincronica

--- controllers/consultasController.js
+++ controllers/consultasController.js
@@ -2,25 +2,20 @@ import consultasModel from "../models/consultasModel.js";
 
 const consultasController = {};
 
-consultasController.inicioConsultas = (req, res) => {
+consultasController.inicioConsultas = async (req, res) => {
     const idVeterinaria = req.session.user.id; 
 
-    consultasModel.consultaInicio(idVeterinaria, (error, results) => {
-        if (error) {
-            console.error("Error en el controlador de consultas:", error);
-            res.redirect('/?error=internalError');
-        }
+    try{
+        const results = await consultasModel.consultaInicio(idVeterinaria)
+        
+        res.render("consultas", {
+            datosConsultaGeneral: results.resultsConsultaGeneral,
+            datosConsultaVacunacion: results.resultsConsultaVacunacion,
+        });
 
-        try {
-            res.render("consultas", {
-                datosConsultaGeneral: results.datosConsultaGeneral,
-                datosConsultaVacunacion: results.datosConsultaVacunacion,
-            });
-        } catch (err) {
-            console.error("Error al renderizar la vista de consultas:", err);
-            res.redirect('/?error=internalError');
-        }
-    });
+    }catch(error){
+        console.log("Error en el controller consultas")
+    }
 };
 
 export default consultasController;
--- models/consultasModel.js
+++ models/consultasModel.js
@@ -2,59 +2,39 @@ import pool from "../config/conexion.js";
 
 const consultasModel = {}
 
-consultasModel.consultaInicio = (idVeterinaria) => {
-    const queryConsultaGeneral = new Promise((resolve, reject) => {
-        const peticionDatos = `
-            SELECT 
-                tb_consultaGeneral_col_fecha,
-                tb_consultaGeneral_col_nombrePropietario,
-                tb_consultaGeneral_col_nombrePaciente,
-                tb_consultaGeneral_col_motivo,
-                tb_consultaGeneral_col_medicamentosUtilizados,
-                tb_consultaGeneral_col_actualizacionPeso
-            FROM 
-                tb_consultageneral
-            WHERE 
-                tb_pacientes_idtb_pacientes IS NULL 
-                AND tb_usuariosVeterinaria_idtb_usuariosVeterinaria = ?;`;
-        
-                pool.query(peticionDatos, [idVeterinaria], (error, results) => {
-            if (error) return reject(error);
-            resolve(results);
-        });
-    });
+consultasModel.consultaInicio = async (idVeterinaria) => {
 
-    const queryConsultaVacunacion = new Promise((resolve, reject) => {
-        const peticionDatos = `
-            SELECT 
-                tb_consultaVacunacion_col_fecha,
-                tb_consultaVacunacion_col_nombrePaciente,
-                tb_consultaVacunacion_col_nombrePropietario,
-                tb_consultaVacunacion_col_vacunacion,
-                tb_consultaVacunacion_col_desparacitacion,
-                tb_consultaVacunacion_col_actualizacionPeso
-            FROM 
-                tb_consultavacunacion
-            WHERE 
-                tb_pacientes_idtb_pacientes IS NULL 
-                AND tb_usuariosVeterinaria_idtb_usuariosVeterinaria = ?;`;
-        
-                pool.query(peticionDatos, [idVeterinaria], (error, results) => {
-            if (error) return reject(error);
-            resolve(results);
-        });
-    });
+    try{
 
-    Promise.all([queryConsultaGeneral, queryConsultaVacunacion])
-        .then(results => {
-            callback(null, {
-                datosConsultaGeneral: results[0],
-                datosConsultaVacunacion: results[1]
-            });
-        })
-        .catch(error => {
-            callback(error);
-        });
+        const peticionConsultaGeneral = `SELECT 
+        tb_consultaGeneral_col_fecha, tb_consultaGeneral_col_nombrePropietario,
+        tb_consultaGeneral_col_nombrePaciente, tb_consultaGeneral_col_motivo,
+        tb_consultaGeneral_col_medicamentosUtilizados, tb_consultaGeneral_col_actualizacionPeso
+        FROM tb_consultageneral
+        WHERE 
+        tb_pacientes_idtb_pacientes IS NULL 
+        AND tb_usuariosVeterinaria_idtb_usuariosVeterinaria = ?;`;
+
+        const peticionConsultaVacunacion = `SELECT 
+        tb_consultaVacunacion_col_fecha, tb_consultaVacunacion_col_nombrePaciente,
+        tb_consultaVacunacion_col_nombrePropietario, tb_consultaVacunacion_col_vacunacion, 
+        tb_consultaVacunacion_col_desparacitacion, tb_consultaVacunacion_col_actualizacionPeso
+        FROM tb_consultavacunacion
+        WHERE 
+        tb_pacientes_idtb_pacientes IS NULL 
+        AND tb_usuariosVeterinaria_idtb_usuariosVeterinaria = ?;`;
+
+
+        const [resultsConsultaGeneral] = await pool.execute(peticionConsultaGeneral, [idVeterinaria])
+        const [resultsConsultaVacunacion] = await pool.execute(peticionConsultaVacunacion, [idVeterinaria])
+
+        return{
+            resultsConsultaGeneral, resultsConsultaVacunacion
+        }
+    }catch(error){
+        console.log("Error modelo consultas/peticion de datos", error)
+    }
+
 };
 
 export default consultasModel;
